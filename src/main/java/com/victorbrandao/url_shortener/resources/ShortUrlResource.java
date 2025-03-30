769cfabd106219c278e52d810f5f5eb20f52e8dd
Victor <victor_brandao@outlook.com>
2025-03-29 23:11:42 -0300

    feat: ShortUrl layers

--- src/main/java/com/victorbrandao/url_shortener/resources/ShortUrlResource.java
+++ src/main/java/com/victorbrandao/url_shortener/resources/ShortUrlResource.java
@@ -1,13 +1,19 @@
 package com.victorbrandao.url_shortener.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.victorbrandao.url_shortener.entities.ShortUrl;
 import com.victorbrandao.url_shortener.services.ShortUrlService;
@@ -28,6 +34,30 @@ public class ShortUrlResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ShortUrl> findById(@PathVariable Long id) {
 		ShortUrl obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ShortUrl> insert(@RequestBody ShortUrl obj) {
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ShortUrl> update(@PathVariable Long id, @RequestBody ShortUrl obj) {
+		obj = service.update(id, obj);
+		
 		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/victorbrandao/url_shortener/services/ShortUrlService.java
+++ src/main/java/com/victorbrandao/url_shortener/services/ShortUrlService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.victorbrandao.url_shortener.entities.ShortUrl;
 import com.victorbrandao.url_shortener.repositories.ShortUrlRepository;
+import com.victorbrandao.url_shortener.services.exceptions.DatabaseException;
+import com.victorbrandao.url_shortener.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ShortUrlService {
@@ -22,5 +28,38 @@ public class ShortUrlService {
 	public ShortUrl findById(Long id) {
 		Optional<ShortUrl> obj = repository.findById(id);
 		return obj.get();
+	}
+	
+	public ShortUrl insert(ShortUrl obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			if (!repository.existsById(id))
+				throw new ResourceNotFoundException(id);
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public ShortUrl update(Long id, ShortUrl obj) {
+		try {
+			ShortUrl entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+				
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(ShortUrl entity, ShortUrl obj) {
+		entity.setIdentifier(obj.getIdentifier());
+		entity.seturlOriginal(obj.geturlOriginal());
+		entity.seturlShortened(obj.geturlShortened());
 	}
 }
