f7b2178760afc829f7a1b672f82432b2b03f0b3d
Victor <victor_brandao@outlook.com>
2025-11-05 11:34:18 -0300

    refactor - removida declaração desnecessária (UserDetails herda Serializable)

--- src/main/java/com/victorbrandao/url_shortener/entities/User.java
+++ src/main/java/com/victorbrandao/url_shortener/entities/User.java
@@ -1,6 +1,5 @@
 package com.victorbrandao.url_shortener.entities;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -20,7 +19,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements UserDetails, Serializable  {
+public class User implements UserDetails  {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
